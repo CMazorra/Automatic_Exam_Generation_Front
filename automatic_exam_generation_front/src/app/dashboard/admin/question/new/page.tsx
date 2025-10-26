c8f5962d76342a67baa6b0b2bb5900a8c7a30815
D4R102004 <alfonsodario54@gmail.com>
2025-10-26 14:39:17 -0400

    Refactor: Separar formularios de creación de entidades, eliminar campos de ID y mejorar navegación desde list views

---
+++ automatic_exam_generation_front/src/app/dashboard/admin/question/new/page.tsx
@@ -0,0 +1,9 @@
+import { CreateQuestionForm } from "@/components/create-entities"
+
+export default function Page() {
+  return (
+    <main className="p-6">
+      <CreateQuestionForm />
+    </main>
+  )
+}
